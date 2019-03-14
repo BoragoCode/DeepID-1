545e48675e89a2dc065633899dfc142950122b2e
louishsu <is.louishsu@foxmail.com>
2019-03-14 14:26:29 +0800

    modify config

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ from easydict import EasyDict
 
 configer = EasyDict()
 
-configer.patch = 0
-configer.scale = 'M'
+configer.patch = 0              # 0 ~ 8
+configer.scale = 'M'            # 'S', 'M', 'L'
 
 if configer.patch == 0:
     configer.imsize = (44, 33)  # h: w = 4: 3
@@ -11,11 +11,19 @@ elif configer.patch in [1, 2, 3]:
     configer.imsize = (25, 33)  # h: w = 3: 4
 else:
     configer.imsize = (25, 25)  # h: w = 1: 1
-    
-configer.n_classes = 5749
 
-configer.modelname = 'classify_patch{}_scale{}'.\
-                        format(configer.patch, configer.scale)
+configer.in_channels = 3
+configer.n_classes = 5749
+configer.modelname = 'classify_patch{}_scale{}'.format(configer.patch, configer.scale)
+configer.modeldir  = '../modelfile/{}'.format(configer.modelname)
+configer.logdir    = '../logfile/{}'.format(configer.modelname)
+
 configer.lrbase = 0.001
-configer.batchsize = 64
+configer.stepsize = 5
+configer.gamma = 0.9
+
+configer.verify_weight = 0.1 
+
+configer.batchsize = 256
 configer.n_epoch = 300
+configer.valid_batch = 200
